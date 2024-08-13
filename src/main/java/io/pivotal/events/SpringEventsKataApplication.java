b2ac685cc47f9cc4773fa4b9e4ad929ef31d0798
Keith Kester <keith.a.kester.ctr@army.mil>
2024-08-13 09:10:24 -0500

    rename

--- src/main/java/io/pivotal/events/SpringEventsKataApplication.java
+++ src/main/java/io/pivotal/events/SpringEventsKataApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJpaKataApplication {
+public class SpringEventsKataApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJpaKataApplication.class, args);
+		SpringApplication.run(SpringEventsKataApplication.class, args);
 	}
 }
